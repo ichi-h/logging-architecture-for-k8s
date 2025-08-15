95a9f93155f11a9add7c580b4a368653c8f07dca
ichi-h <85932406+ichi-h@users.noreply.github.com>
2025-08-15 14:06:55 +0900

    Add Helmfile

--- flake.nix
+++ flake.nix
@@ -9,16 +9,47 @@
   outputs = { self, nixpkgs, flake-utils }: 
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        overlays = [
+          (final: prev: rec {
+            kubernetes-helm-wrapped = prev.wrapHelm prev.kubernetes-helm {
+              plugins = with prev.kubernetes-helmPlugins; [
+                helm-diff
+              ];
+            };
+          })
+        ];
+        pkgs = import nixpkgs { inherit system overlays; };
       in
       {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             minikube
             kubectl
-            kubernetes-helm
+            kubernetes-helm-wrapped
+            helmfile-wrapped
           ];
-          shellHook = builtins.readFile ./scripts/setup.sh;
+          shellHook = ''
+            if ! minikube status | grep -q "Running"; then
+              echo "Starting minikube..."
+              minikube start
+            else
+              echo "minikube is already running."
+            fi
+
+            if ! minikube addons list | grep -q "dashboard.*enabled"; then
+              echo "Enabling dashboard..."
+              minikube addons enable dashboard
+            else
+              echo "dashboard is already enabled."
+            fi
+
+            if ! minikube addons list | grep -q "metrics-server.*enabled"; then
+              echo "Enabling metrics-server..."
+              minikube addons enable metrics-server
+            else
+              echo "metrics-server is already enabled."
+            fi
+          '';
         };
       }
     );
