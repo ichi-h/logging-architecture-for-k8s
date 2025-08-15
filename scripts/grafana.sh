21a488ff847bbdc0e7a8f2ed9214746b08b4fe5b
ichi-h <85932406+ichi-h@users.noreply.github.com>
2025-08-15 19:35:44 +0900

    rename and update namespace and grafana command

--- scripts/grafana.sh
+++ scripts/grafana.sh
@@ -4,12 +4,7 @@ if [ -z "$port" ]; then
   exit 1
 fi
 
-ipaddress=`minikube ip`
-if [ -z "$ipaddress" ]; then
-  echo "Minikube IP address not found. Ensure Minikube is running."
-  exit 1
-fi
-
-echo Origin: http://$ipaddress:$port
+echo Origin: http://localhost:$port
 echo Username: admin
 echo Password: $(kubectl get secret --namespace monitoring grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo)
+kubectl port-forward svc/grafana 30434:80 -n monitoring
